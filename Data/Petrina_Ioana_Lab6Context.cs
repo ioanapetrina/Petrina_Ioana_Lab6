c2773f57cad84b8f3b47f34dcd74c1854e9df905
IOANA PETRINA <ioana.petrina1@stud.ubbcluj.ro>
2021-01-10 15:38:55 +0200

    Add project files.

---
+++ Data/Petrina_Ioana_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Petrina_Ioana_Lab6.Models;
+
+namespace Petrina_Ioana_Lab6.Data
+{
+    public class Petrina_Ioana_Lab6Context : DbContext
+    {
+        public Petrina_Ioana_Lab6Context (DbContextOptions<Petrina_Ioana_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Petrina_Ioana_Lab6.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Petrina_Ioana_Lab6.Data;
+using Petrina_Ioana_Lab6.Models;
+
+namespace Petrina_Ioana_Lab6.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Petrina_Ioana_Lab6.Data.Petrina_Ioana_Lab6Context _context;
+
+        public DetailsModel(Petrina_Ioana_Lab6.Data.Petrina_Ioana_Lab6Context context)
+        {
+            _context = context;
+        }
+
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
